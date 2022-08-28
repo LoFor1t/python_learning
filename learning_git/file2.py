c265b7455d4ed061d94b390f071ed142ca64affb
LoFor1t <vgresnyh@gmail.com>
2022-08-28 12:54:18 +0300

    Add new module hello_git.py and add function multi() in file2.py

---
+++ learning_git/file2.py
@@ -0,0 +1,10 @@
+def up(text):
+    return text.upper()
+
+
+def main():
+    print(up('hello'))
+
+
+if __name__ == '__main__':
+    main()
--- learning_git/hello_git.py
+++ learning_git/hello_git.py
@@ -2,3 +2,10 @@ print('Hello GIT!')
 print('Wow, you have a very beautiful theme!')
 print('Hello!')
 print('Hello!')
+
+
+def multi(a, b):
+    return a * b
+
+
+print(multi(1, 5))
