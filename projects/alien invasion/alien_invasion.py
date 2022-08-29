510c9775bc17f8af01b78be80f39432c3bfa42ac
LoFor1t <vgresnyh@gmail.com>
2022-08-29 09:58:35 +0300

    Сделал рефактор методов _check_events() и _update_screen()

--- projects/alien invasion/alien_invasion.py
+++ projects/alien invasion/alien_invasion.py
@@ -22,17 +22,21 @@ class AlienInvasion:
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            # Отслеживает события клавиатуры и мыши
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Отображение последнего прорисованного кадра.
-            pygame.display.flip()
+    def _update_screen(self):
+        """Обновляет изображение на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
