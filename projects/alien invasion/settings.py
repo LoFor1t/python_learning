410b0d88e463dd935ea9b4409b00eab2e6e22e43
LoFor1t <vgresnyh@gmail.com>
2022-08-29 11:53:02 +0300

    Добавил возможность менять скорость корабля в файле settings.py

--- projects/alien invasion/settings.py
+++ projects/alien invasion/settings.py
@@ -1,5 +1,5 @@
 class Settings:
-    """Класс для хранения всех настроек игры  Alien Invasion."""
+    """Класс для хранения всех настроек игры Alien Invasion."""
 
     def __init__(self):
         """Инициализируем настройки игры."""
@@ -7,3 +7,6 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
+
+        # Настройки корабля.
+        self.ship_speed = 1.5
