1d8f084a7e910eb76c6afccb3817ed26e0c14be7
LoFor1t <vgresnyh@gmail.com>
2022-08-30 19:27:25 +0300

    1. В файл настроек добавил переменную содержащую максимальное количество снарядов, которые могут находиться на одном экране. 2. В основном файле игры добавил в метод _fire_bullet() проверку количества снарядов находящихся на экране. Если меньше 3-х - можно создать.

--- projects/alien invasion/alien_invasion.py
+++ projects/alien invasion/alien_invasion.py
@@ -38,7 +38,6 @@ class AlienInvasion:
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom < 0:
                     self.bullets.remove(bullet)
-                    print(len(self.bullets))
 
             self._update_screen()
 
@@ -72,8 +71,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Создание нового снаряда и включение его в группу bullets."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран."""
